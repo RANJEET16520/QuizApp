359ba3c8f1343c4562f65b08f77a493fe0a7ed0b
shkamboj <iiitu16118@gmail.com>
2018-08-31 02:37:06 +0530

    database

--- index.js
+++ index.js
@@ -39,8 +39,6 @@ mongodb.MongoClient.connect(uri, function(err, client) {
    * new database, it is created automatically when we insert.
    */
 
-  let db = client.db('quizapp')
-
   /*
    * First we'll add a few songs. Nothing is required to create the
    * songs collection; it is created automatically when we insert.
@@ -50,51 +48,7 @@ mongodb.MongoClient.connect(uri, function(err, client) {
 
    // Note that the insert method can take either an array or a dict.
 
-  songs.insert(seedData, function(err, result) {
-
-    if(err) throw err;
-
-    /*
-     * Then we need to give Boyz II Men credit for their contribution
-     * to the hit "One Sweet Day".
-     */
-
-    songs.update(
-      { song: 'One Sweet Day' },
-      { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
-      function (err, result) {
-
-        if(err) throw err;
-
-        /*
-         * Finally we run a query which returns all the hits that spend 10 or
-         * more weeks at number 1.
-         */
-
-        songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
-
-          if(err) throw err;
-
-          docs.forEach(function (doc) {
-            console.log(
-              'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
-              ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
-            );
-          });
-
-          // Since this is an example, we'll clean up after ourselves.
-          songs.drop(function (err) {
-            if(err) throw err;
-
-            // Only close the connection when your app is terminating.
-            client.close(function (err) {
-              if(err) throw err;
-            });
-          });
-        });
-      }
-    );
-  });
+  songs.insert(seedData);
 });
 
 
