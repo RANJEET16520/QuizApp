7a87738092999d2e1c7667a3f9ba05489db171dd
shkamboj <iiitu16118@gmail.com>
2018-09-01 23:45:32 +0530

    passport code removed

--- index.js
+++ index.js
@@ -2,52 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var mongoose = require('mongoose');
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongo = require('mongodb');
-
-app.use(session({
-	secret: 'secret',
-	saveUninitialized: true,
-	resave: true
-}));
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-app.use(expressValidator({
-	errorFormatter: function(param, msg, value){
-		var namespace = param.split('.'),
-		root = namespace.shift(),
-		formParam = root;
-
-	while(namespace.length){
-		formParam += '[' + namespace.shift() + ']';
-	}
-	return {
-		param : formParam,
-		msg : msg,
-		value :value
-	};
-	}
-}));
-
-app.use(flash());
-
-app.use(function(req, res, next){
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	res.locals.error = req.flash('error');
-	next();
-});
-
-
 
 var personSchema = mongoose.Schema({
     rollno:{type: String,
@@ -61,9 +15,6 @@ var personSchema = mongoose.Schema({
       required: true
     }
 });
-
-
-
 var Person = mongoose.model("Person", personSchema);
 
 
