c5f75c7530082838cd9d321f34118814674adafe
shkamboj <iiitu16118@gmail.com>
2019-01-10 00:42:34 +0530

    changes

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require('bcrypt-nodejs');
 var crypto = require('crypto');
 var passportfb = require('passport-facebook');
-var quizzer = require('node-quizzer');
 var _sh = require('underscore-node');
 
 
